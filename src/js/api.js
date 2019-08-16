9a8f946f97f93e8ea1dde5255fc748a2a5865db9
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-08-16 10:18:53 +0300

    fetch added

--- src/js/api.js
+++ src/js/api.js
@@ -10,3 +10,7 @@ export function getSingleFilmActors(id) {
   const url = `${baseUrl}/movie/${id}?api_key=${api_key}&append_to_response=credits`;
   return fetch(url).then(res => res.json());
 }
+export function getPopularFilms() {
+  const url = `${baseUrl}/movie/popular?api_key=${api_key}&append_to_response=credits`;
+  return fetch(url).then(res => res.json());
+}
--- src/page-about/film-data.js
+++ src/page-about/film-data.js
@@ -1,31 +1,31 @@
-import {getSingleFilmTrailer} from '../js/api';
-import {getSingleFilmActors} from '../js/api';
-import actors from '../page-about/actors.hbs';
+import { getSingleFilmTrailer } from "../js/api";
+import { getSingleFilmActors } from "../js/api";
+import actors from "../page-about/actors.hbs";
 
 class FilmData {
   constructor(id) {
     this.filmId = id;
     this.api_key = "ed5781108818e96397f9efe7bddd0923";
     this.refs = {
-      iframeTrailer: document.querySelector('.iframe_trailer'),
-      ulActors: document.querySelector('.actors-list'),
-    }
+      iframeTrailer: document.querySelector(".iframe_trailer"),
+      ulActors: document.querySelector(".actors-list")
+    };
     this.renderActors();
   }
 
   renderTrailer() {
     getSingleFilmTrailer(this.filmId).then(data => {
       const trailerKey = data.results[0].key;
-      this.refs.iframeTrailer.src = 'http://www.youtube.com/embed/' + trailerKey;
+      this.refs.iframeTrailer.src =
+        "http://www.youtube.com/embed/" + trailerKey;
     });
-
   }
 
   renderActors() {
     getSingleFilmActors(this.filmId).then(data => {
       const markup = actors(data.credits.cast);
-      this.refs.ulActors.insertAdjacentHTML('afterbegin', markup);
-    })
+      this.refs.ulActors.insertAdjacentHTML("afterbegin", markup);
+    });
   }
 }
 
