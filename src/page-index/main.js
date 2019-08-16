fd354af571c7aa51d10341a939f62db74be9e058
Pushtaryk Oleksii <pushtarik@gmail.come>
2019-08-16 16:31:27 +0300

    changes

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -22,7 +22,9 @@ class Mooogle {
     );
 
     // обработчик на клик по модалке "search"
-    this.fnClickCloseSearchBlockHandler = function clickCloseSearchBlockHandler(e) {
+    this.fnClickCloseSearchBlockHandler = function clickCloseSearchBlockHandler(
+      e
+    ) {
       if (e.target.className !== "search_modal") {
         return;
       }
@@ -42,49 +44,44 @@ class Mooogle {
     // ===============
     // Oleksii
     this.filmsList = document.querySelector(".films-list");
-    // this.buttonStar = document.querySelector(".button_icon-star");
-    // this.buttonBell = document.querySelector(".button_icon-bell");
-    // this.fill = document.querySelector(".fill-color");
-    // this.iconStar = document.querySelector(".icon-star");
+    this.buttonStar = document.querySelector(".button_icon-star");
+    this.buttonBell = document.querySelector(".button_icon-bell");
+    this.Star = document.querySelector(".icon-star");
+    this.Bell = document.querySelector(".icon-bell");
+    this.fill = document.querySelector(".fill-color");
+    this.iconStar = document.querySelector(".icon-star");
 
+    
 
     this.renderFilms();
-    this.filmsList.addEventListener('click', event => {
+    this.filmsList.addEventListener("click", event => {
+      if ((e.target.nodeName = "LI")) {
+        console.dir(event.target.dataset.id);
 
-      if(e.target.nodeName = "LI") {
-
-        console.dir(event.target);
-        localStorage.setItem('id', event.target.dataset.id);
+        localStorage.setItem("id", event.target.dataset.id);
       }
-
-      localStorage.setItem('id');
-
+      localStorage.setItem("id");
     });
-
     // Olecsey
   }
   // =========================
   // Oleg
   // обработчик открытия модального окна "search"
   openSearchBlockHandler() {
-
-    window.addEventListener('keydown', this.clickOnEsc);
-    window.addEventListener('click', this.clickOnVoid);
-    this.searchBlock.classList.add('open_search');
+    window.addEventListener("keydown", this.clickOnEsc);
+    window.addEventListener("click", this.clickOnVoid);
+    this.searchBlock.classList.add("open_search");
 
     // this.searchInput.setAttribute('autofocus', true);
     this.searchInput.focus();
-
   }
 
   // обработчик закрытия модального окна "search"
   closeSearchBlockHandler() {
-
-    this.searchBlock.classList.remove('open_search');
+    this.searchBlock.classList.remove("open_search");
     this.searchInput.blur();
-    window.removeEventListener('keydown', this.clickOnEsc);
-    window.removeEventListener('click', this.clickOnVoid);
-
+    window.removeEventListener("keydown", this.clickOnEsc);
+    window.removeEventListener("click", this.clickOnVoid);
   }
   // Oleg
   // ================
