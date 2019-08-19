541c7d5700c515d6075d5be4228769db7f8977a6
Kh-Ol-An <olegkhristenko@gmail.com>
2019-08-19 16:48:11 +0300

    monday evening

--- src/page-about/main.js
+++ src/page-about/main.js
@@ -1,9 +1,13 @@
 import '../scss/main.scss';
 import './page.scss';
 import '../scss/header.scss';
+import '../scss/search-form.scss';
 import './film-data';
 import './film-data-1';
 import 'slick-carousel/slick/slick.scss';
 import 'slick-carousel/slick/slick-theme.scss';
+import '../page-index/main.js';
+
+// new Mooogle()  
 
 
--- src/page-index/main.js
+++ src/page-index/main.js
@@ -8,32 +8,111 @@ import { getPopularTvShows } from '../js/api';
 
 class Mooogle {
   constructor() {
-    // ====================
-    // Oleg
+    // при загрузке страницы рендерит популярные фильмы
+    this.renderPopularFilms();
+
+    // привязки к HTML
     this.refs = {
       // модальное окно "search"
-      searchBlock: document.querySelector(".search_block"),
+      searchBlock: document.querySelector('.search_block'),
       // кнопка вызова модального окна
-      btnCallSearchModal: document.querySelector(".search-engine"),
+      btnCallSearchModal: document.querySelector('.search-engine'),
       // поле "input"
-      searchInput: document.querySelector("#search_input"),
+      searchInput: document.querySelector('#search_input'),
       // кнопка "search"
-      searchForm: document.querySelector("#search_form"),
+      searchForm: document.querySelector('#search_form'),
       // страж для скрола
-      sentinal: document.querySelector("#sentinal"),
+      sentinal: document.querySelector('#sentinal'),
       // список "ul" в "grid"
-      filmsList: document.querySelector(".films-list"),
-      buttonTvShow: document.querySelector(".menu-items-click--tv"),
-      buttonFilm: document.querySelector(".menu-items-click--film"),
-      headerButtonFilm: document.querySelector(".header-items-click--film"),
-      headerButtonTvShow: document.querySelector(".header-items-click--tv")
+      filmsList: document.querySelector('.films-list'),
+      // кнопка "scroll up"
+      scrollUpBtn: document.querySelector('#scroll_up'),
+      buttonTvShow: document.querySelector('.menu-items-click--tv'),
+      buttonFilm: document.querySelector('.menu-items-click--film'),
+      headerButtonFilm: document.querySelector('.header-items-click--film'),
+      headerButtonTvShow: document.querySelector('.header-items-click--tv')
     };
 
     // слушатель на кнопку вызова модального окна
     this.refs.btnCallSearchModal.addEventListener(
-      "click",
+      'click',
       this.openSearchBlockHandler.bind(this)
     );
+
+    // слушатель на кнопке "scroll up"
+    this.refs.scrollUpBtn.addEventListener(
+      'click',
+      this.scrollToUpHandler.bind(this)
+    );
+
+    // слушатель на
+    this.refs.buttonTvShow.addEventListener('click', event => {
+      event.preventDefault();
+      if (event.target === event.currentTarget) {
+        newApp.openPreloader();
+        api.resetPage();
+        this.clearList();
+        this.killInfinityScroll();
+        this.renderTvShows();
+        localStorage.setItem('mediaType', 'TV');
+        show();
+        newApp.closePreloader();
+      }
+    });
+
+    // слушатель на
+    this.refs.headerButtonTvShow.addEventListener('click', event => {
+      event.preventDefault();
+      if (event.target === event.currentTarget) {
+        newApp.openPreloader();
+        api.resetPage();
+        this.clearList();
+        this.killInfinityScroll();
+        this.renderTvShows();
+        localStorage.setItem('mediaType', 'TV');
+        newApp.closePreloader();
+      }
+    });
+
+    // слушатель на
+    this.refs.buttonFilm.addEventListener('click', event => {
+      event.preventDefault();
+      if (event.target === event.currentTarget) {
+        newApp.openPreloader();
+        api.resetPage();
+        this.clearList();
+        this.killInfinityScroll();
+        this.renderPopularFilms();
+        localStorage.setItem('mediaType', 'movie');
+        show();
+        newApp.closePreloader();
+      }
+    });
+
+    // слушатель на
+    this.refs.headerButtonFilm.addEventListener('click', event => {
+      event.preventDefault();
+      if (event.target === event.currentTarget) {
+        newApp.openPreloader();
+        api.resetPage();
+        this.clearList();
+        this.killInfinityScroll();
+        this.renderPopularFilms();
+        localStorage.setItem('mediaType', 'movie');
+        newApp.closePreloader();
+      }
+    });
+
+    // слушатель на
+    this.refs.filmsList.addEventListener('click', event => {
+      preventDefault();
+      if (event.target !== event.currentTarget) {
+        console.log(event.target.dataset.id);
+        localStorage.setItem('id', event.target.dataset.id);
+
+        localStorage.setItem('mediaType', 'movie');
+      }
+    });
 
     // обработчик поиска
     this.searchingHandler = function(e) {
@@ -45,10 +124,9 @@ class Mooogle {
       api.resetPage();
       this.clearList();
       this.killInfinityScroll();
-      this.refs.filmsList.innerHTML = "";
       this.renderSearchingFilm();
       this.closeSearchBlockHandler();
-      input.value = "";
+      input.value = '';
       newApp.closePreloader();
     };
     this.clickOnSearchBtn = this.searchingHandler.bind(this);
@@ -57,11 +135,12 @@ class Mooogle {
     this.onEnt = function(e) {
       if (e[0].isIntersecting) {
         if (api.query === '') {
+          this.refs.scrollUpBtn.classList.remove('is-hidden');
           this.renderPopularFilms();
-          console.log("popular");
+          console.log('popular');
         } else {
           this.renderSearchingFilm();
-          console.log("search");
+          console.log('search');
         }
         this.killInfinityScroll();
       }
@@ -69,7 +148,7 @@ class Mooogle {
     this.onEntry = this.onEnt.bind(this);
     this.infScrl = function() {
       const observOptions = {
-        rootMargin: "100px"
+        rootMargin: '100px'
       };
       this.observer = new IntersectionObserver(this.onEntry, observOptions);
       this.observer.observe(this.refs.sentinal);
@@ -87,15 +166,15 @@ class Mooogle {
         return el;
       });
       const markup = filmsTemplate(this.newArrRes);
-      this.refs.filmsList.insertAdjacentHTML("beforeend", markup);
-      console.log("api.page :", api.page);
+      this.refs.filmsList.insertAdjacentHTML('beforeend', markup);
+      console.log('api.page :', api.page);
       api.increment();
     };
     this.builderListItemOnPageIndex = this.insertListItem.bind(this);
 
     // обработчик на клик по модалке
     this.clickCloseSearchBlockHandler = function(e) {
-      if (e.target.className !== "search_modal") {
+      if (e.target.className !== 'search_modal') {
         return;
       }
       this.closeSearchBlockHandler();
@@ -104,86 +183,30 @@ class Mooogle {
 
     // обработчик на клик по "Esc"
     this.keyPressHandle = function(e) {
-      if (e.code !== "Escape") {
+      if (e.code !== 'Escape') {
         return;
       }
       this.closeSearchBlockHandler();
     };
     this.clickOnEsc = this.keyPressHandle.bind(this);
-    // Oleg
-    // ===============
-    // Oleksii
-
-    this.renderPopularFilms();
-
-    this.refs.buttonTvShow.addEventListener("click", event => {
-      event.preventDefault();
-      if (event.target === event.currentTarget) {
-        this.clearList();
-        this.renderTvShows();
-        localStorage.setItem("mediaType", "TV");
-        show();
-      }
-    });
-    this.refs.headerButtonTvShow.addEventListener("click", event => {
-      event.preventDefault();
-      if (event.target === event.currentTarget) {
-        this.clearList();
-        this.renderTvShows();
-        localStorage.setItem("mediaType", "TV");
-      }
-    });
-
-
-    this.refs.buttonFilm.addEventListener("click", event => {
-      event.preventDefault();
-
-      if (event.target === event.currentTarget) {
-        this.clearList();
-        this.renderPopularFilms();
-        localStorage.setItem("mediaType", "movie");
-        show();
-      }
-    });
-
-    this.refs.headerButtonFilm.addEventListener("click", event => {
-      event.preventDefault();
-      if (event.target === event.currentTarget) {
-        this.clearList();
-        this.renderPopularFilms();
-        localStorage.setItem("mediaType", "movie");
-      }
-    });
-
-    this.refs.filmsList.addEventListener("click", event => {
-      preventDefault();
-      if (event.target !== event.currentTarget) {
-        console.log(event.target.dataset.id);
-        localStorage.setItem("id", event.target.dataset.id);
-
-        localStorage.setItem("mediaType", "movie");
-
-      }
-    });
-    // Olecsey
   }
+
   // ТЕЛО КЛАССА
-  // =========================
-  // Oleg
+
   // обработчик открытия модального окна "search"
   openSearchBlockHandler() {
-    window.addEventListener("keydown", this.clickOnEsc);
-    window.addEventListener("click", this.clickOnVoid);
+    window.addEventListener('keydown', this.clickOnEsc);
+    window.addEventListener('click', this.clickOnVoid);
 
-    this.refs.searchBlock.classList.add("open_search");
+    this.refs.searchBlock.classList.add('open_search');
     this.refs.searchInput.focus();
-    this.refs.searchForm.addEventListener("submit", this.clickOnSearchBtn);
+    this.refs.searchForm.addEventListener('submit', this.clickOnSearchBtn);
   }
 
   // обработчик закрытия модального окна "search"
   closeSearchBlockHandler() {
-    this.refs.searchBlock.classList.remove("open_search");
-    this.refs.searchForm.removeEventListener("submit", this.clickOnSearchBtn);
+    this.refs.searchBlock.classList.remove('open_search');
+    this.refs.searchForm.removeEventListener('submit', this.clickOnSearchBtn);
 
     window.removeEventListener('keydown', this.clickOnEsc);
     window.removeEventListener('click', this.clickOnVoid);
@@ -206,9 +229,6 @@ class Mooogle {
       });
   }
 
-  // Oleg
-  // ================
-  // Olecsey
   // Рендеринг популярных фильмов
   renderPopularFilms() {
     api
@@ -224,19 +244,32 @@ class Mooogle {
       .catch(error => console.warn(error));
   }
 
+  // Рендеринг TV сериалов
   renderTvShows() {
-    getPopularTvShows().then(data => {
-      this.builderListItemOnPageIndex(data);
+    getPopularTvShows()
+      .then(data => {
+        if (data.total_pages < api.page) {
+          this.killInfinityScroll();
+          return;
+        }
+        this.builderListItemOnPageIndex(data);
+        this.infinityScroll();
+      })
+      .catch(error => console.warn(error));
+  }
+
+  // очистка HTML
+  clearList() {
+    this.refs.filmsList.innerHTML = '';
+  }
+
+  // обработчик на кнопку "scroll up"
+  scrollToUpHandler() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
   }
-
-  // Oleksii==========
-  clearList() {
-    this.refs.filmsList.innerHTML = "";
-  }
-
-  // Olecsey
-  // ===============
 }
 
 const newMooogle = new Mooogle();
@@ -246,8 +279,8 @@ const newMooogle = new Mooogle();
 const sidebarShow = document.querySelector('.toggle-btn');
 sidebarShow.addEventListener('click', show);
 function show() {
-  document.getElementById("sidebar").classList.toggle("active");
-  document.body.classList.toggle("modal-overlay-menu");
+  document.getElementById('sidebar').classList.toggle('active');
+  document.body.classList.toggle('modal-overlay-menu');
 }
 // Vica
 // ======================
