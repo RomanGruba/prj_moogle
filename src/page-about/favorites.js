1a9c2f5737ef02c731003e10b382b6828c9f7bdd
Roman Gruba <roman.gruba@gmail.com>
2019-08-20 11:56:02 +0300

    spinner fixed

--- src/page-about/favorites.js
+++ src/page-about/favorites.js
@@ -1,4 +1,4 @@
-export function setIdToLocalSotage() {
-  const favoritesArr = [429203, 384018, 363088, 448358];
-  localStorage.setItem("favorites", JSON.stringify(favoritesArr));
-}
+// export function setIdToLocalSotage() {
+//   const favoritesArr = [429203, 384018, 363088, 448358];
+//   localStorage.setItem("favorites", JSON.stringify(favoritesArr));
+// }
